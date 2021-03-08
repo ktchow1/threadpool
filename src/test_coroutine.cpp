6e9c652489403037d2e3fc19e67a66995c95e92d
ktchow1 <ktchow1@yahoo.com.hk>
2021-03-08 23:31:43 +0800

    add printout sequence for coroutine

--- src/test_coroutine.cpp
+++ src/test_coroutine.cpp
@@ -8,40 +8,49 @@
 // ****************** // 
 // *** Approach 0 *** //
 // ****************** // 
-struct future_type0 
+struct future0 
 {
-    struct promise_type // This type cannot be changed. 
+    struct promise_type // This is promise_type, NOT promise.
     {
-        future_type0 get_return_object() 
-        {
-            return {}; 
-        }
-
-        std::suspend_never initial_suspend() { return {}; }
-        std::suspend_never   final_suspend() { return {}; }
-        void unhandled_exception()           {            }
+        promise_type()                       { std::cout << "\npromise::promise"; }             // step-1
+        future0 get_return_object()          { std::cout << "\npromise::ret_obj"; return {}; }  // step-2
+        std::suspend_never initial_suspend() { std::cout << "\npromise::initial"; return {}; }  // step-4
+        std::suspend_never   final_suspend() { std::cout << "\npromise::final";   return {}; }
+        void unhandled_exception()           { }
     };
-};
-
-struct awaitable_type0  
-{
-    std::coroutine_handle<>* h_ptr;
-    constexpr bool await_ready()  const noexcept { return false; }
-    constexpr void await_resume() const noexcept {               }
-    void await_suspend(std::coroutine_handle<> h) 
+    
+    future0() // step-3
     {
-        *h_ptr = h;   
-        std::cout << " [await_suspend] " << std::flush; 
+        std::cout << "\nfuture0::future0"; 
     }
 };
 
-future_type0 counter0(std::coroutine_handle<>* h_ptr)
+struct awaitable0
 {
-    awaitable_type0 a{ h_ptr };
+    explicit awaitable0(std::coroutine_handle<>* h_ptr_) : h_ptr(h_ptr_) // step-5 
+    {
+        std::cout << "\nawaitable::awaitable"; 
+    }
+
+    // No printing for constexpr fct.
+    constexpr bool await_ready()  const noexcept { return false; }
+    constexpr void await_resume() const noexcept {               }
+    void await_suspend(std::coroutine_handle<> h) // step-6 and for each co_await called
+    {
+        std::cout << "\nawaitable::suspend(handle)";
+        *h_ptr = h;   
+    }
+
+    std::coroutine_handle<>* h_ptr;
+};
+
+future0 coroutine0(std::coroutine_handle<>* h_ptr)
+{
+    awaitable0 a{ h_ptr };
     for(std::uint32_t n=0;; ++n) 
     {
         co_await a;
-        std::cout << "\ncoroutine0 = " << n << " with thread_id = " << std::this_thread::get_id();
+        std::cout << "\ncoroutine0::iteration " << n << " with thread_id = " << std::this_thread::get_id();
     }
 }
 
@@ -49,37 +58,43 @@ future_type0 counter0(std::coroutine_handle<>* h_ptr)
 // ********************************** // 
 // *** Approach 1 (No await type) *** //
 // ********************************** // 
-struct future_type1 
+struct future1 
 {
-    struct promise_type // This type cannot be changed. 
+    struct promise_type 
     {
-        future_type1 get_return_object() 
-        {
-            return future_type1{      std::coroutine_handle<promise_type>::from_promise(*this) }; 
-        //  return             { .h = std::coroutine_handle<promise_type>::from_promise(*this) }; 
-            // C++20 designated initializer syntax, where . is necessary (both the following work)
-        }
-
-        std::suspend_never initial_suspend() { return {}; }
-        std::suspend_never   final_suspend() { return {}; }
-        void unhandled_exception()           {            }
+        promise_type()                       { std::cout << "\npromise::promise"; } // step-1
+        future1 get_return_object()          { std::cout << "\npromise::ret_obj";   // step-2
+                                               return future1
+                                               { 
+                                                   std::coroutine_handle<promise_type>::from_promise(*this) 
+                                               };
+                                             }
+        std::suspend_never initial_suspend() { std::cout << "\npromise::initial"; return {}; } // step 4
+        std::suspend_never   final_suspend() { std::cout << "\npromise::final";   return {}; }
+        void unhandled_exception()           { }
     };
 
-    // *** Conversion operator *** //
-    operator std::coroutine_handle<promise_type>() const // invoked in caching 
+    future1(std::coroutine_handle<promise_type> h_) : h(h_) // step-3
     {
+        std::cout << "\nfuture1::future1"; 
+    } 
+
+    // *** Conversion operator *** //
+    operator std::coroutine_handle<promise_type>() const // step-5
+    {
+        std::cout << "\nfuture1::convert_to_handle"; 
         return h;
     } 
 
     std::coroutine_handle<promise_type> h;
 };
 
-[[nodiscard]] future_type1 counter1()
+[[nodiscard]] future1 coroutine1()
 {
     for(std::uint32_t n=0;; ++n) 
     {
         co_await std::suspend_always{};
-        std::cout << "\ncoroutine1 = " << n << " with thread_id = " << std::this_thread::get_id();
+        std::cout << "\ncoroutine1::iteration " << n << " with thread_id = " << std::this_thread::get_id();
     }
 }
 
@@ -96,46 +111,25 @@ void test_coroutine()
 {
     // *** Approach 0 *** //
     std::coroutine_handle<> h0;
-    counter0(&h0);
-
-    std::cout << "\ncoroutine0 started with main thread_id = " << std::this_thread::get_id();
-    for(int i=0; i<15; ++i) h0();
-    std::cout << "\n";
+    std::cout << "\n--------------------";
+    coroutine0(&h0);
+    std::cout << "\n--------------------";
+    std::cout << "\ncaller with main thread_id = " << std::this_thread::get_id();
+    for(int i=0; i<10; ++i) h0();
+    std::cout << "\n\n";
 
 
     // *** Approach 1 *** //
-    std::coroutine_handle<> h1 = counter1();
+    std::cout << "\n--------------------";
+    std::coroutine_handle<> h1 = coroutine1();
+    std::cout << "\n--------------------";
+    std::cout << "\ncaller with main thread_id = " << std::this_thread::get_id();
+    for(int i=0; i<10; ++i) h1();
+    std::cout << "\n\n";
 
-    std::cout << "\ncoroutine1 started with main thread_id = " << std::this_thread::get_id();
-    for(int i=0; i<15; ++i) h1();
-    std::cout << "\n";
-
-
+    // *** Explicit destroy handle in heap *** //
     h0.destroy();
     h1.destroy();
 }
 
 
-/*  
-Components involved : 
-1. coroutine
-2. coroutine_handle (housekeeping all coroutine states)
-3. awaitable_object
-4. promise_type + future_type
-
-Requirements :
-1. awaitable_object std::suspend_always and std::suspend_never are provided
-2. awaitable_object (by some means) updates or returns a coroutine handle to coroutine caller
-3. awaitable_object offers 3 functions
-4. define future_type::promise_type exists
-5. define promise_type::get_return_object() returns future_type
-6. coroutine_handle contains an instance of promise_type (with a known offset)
-   using function std::coroutine_handle<promise_type>::from_promise(*this)
-   future_type is returned from coroutine
-   future_type can be converted to std::coroutine_handle<>, please define the conversion
-7. std::coroutine_handle<T> can be converted to std:coroutine_handle<void>  
-   std::coroutine_handle<T> <---> std::promise_type is possible
-   std::coroutine_handle<>  <---> NO ... 
-   both handles can be converted into each other  
-
-*/
