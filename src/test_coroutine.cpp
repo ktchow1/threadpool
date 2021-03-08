4586cef8d4adbe9de1b3039b43b1996b48404c49
ktchow1 <ktchow1@yahoo.com.hk>
2021-03-09 01:56:39 +0800

    attempt to return on resuming coroutine

--- src/test_coroutine.cpp
+++ src/test_coroutine.cpp
@@ -8,15 +8,25 @@
 // ****************** // 
 // *** Approach 0 *** //
 // ****************** // 
+// How does caller get a coroutine_handle? 
+// 1. pass handle pointer from caller to coroutine
+// 2. pass handle pointer from coroutine to awaitable
+// 3. init handle pointer inside awaitable::suspend
+// 
+// How does coroutine get a coroutine_handle?
+// 1. own  handle pointer inside coroutine
+// 2. pass handle pointer from coroutine to awaitable (same as above)
+// 3. init handle pointer inside awaitable::suspend   (same as above)
+
 struct future0 
 {
     struct promise_type // This is promise_type, NOT promise.
     {
-        promise_type()                       { std::cout << "\npromise::promise"; }             // step-1
-        future0 get_return_object()          { std::cout << "\npromise::ret_obj"; return {}; }  // step-2
-        std::suspend_never initial_suspend() { std::cout << "\npromise::initial"; return {}; }  // step-4
-        std::suspend_never   final_suspend() { std::cout << "\npromise::final";   return {}; }
-        void unhandled_exception()           { }
+        promise_type()                       {  std::cout << "\npromise::promise";             } // step-1
+        future0 get_return_object()          {  std::cout << "\npromise::ret_obj"; return {};  } // step-2
+        std::suspend_never initial_suspend() {  std::cout << "\npromise::initial"; return {};  } // step-4
+        std::suspend_never   final_suspend() {  std::cout << "\npromise::final";   return {};  }
+        void unhandled_exception()           {  }
     };
     
     future0() // step-3
@@ -33,13 +43,10 @@ struct awaitable0
     }
 
     // No printing for constexpr fct.
-    constexpr bool await_ready()  const noexcept { return false; }
-    constexpr void await_resume() const noexcept {               }
-    void await_suspend(std::coroutine_handle<> h) // step-6 and for each co_await called
-    {
-        std::cout << "\nawaitable::suspend(handle)";
-        *h_ptr = h;   
-    }
+    bool await_ready()  const noexcept            { std::cout << "\nawait::ready";    return false; } // step-6 before co_await called
+    void await_suspend(std::coroutine_handle<> h) { std::cout << "\nawait::suspend(h)"; *h_ptr = h; } // step-7 before co_await called
+    // ----------------------------------------------------------------------------------------------------------------------------------
+    void await_resume() const noexcept            { std::cout << "\nawait::resume";                 } // step-8 after  co_await resumed 
 
     std::coroutine_handle<>* h_ptr;
 };
@@ -55,26 +62,31 @@ future0 coroutine0(std::coroutine_handle<>* h_ptr)
 }
 
 
-// ********************************** // 
-// *** Approach 1 (No await type) *** //
-// ********************************** // 
+// ****************** // 
+// *** Approach 1 *** //
+// ****************** // 
+// How does caller get a coroutine pointer (with another method)?
+// 1. promise constructs a future object with coroutine_handle inside
+// 2. caller obtains handle from future::get() or 
+//    caller obtains handle from future::conversion_to_handle
+
 struct future1 
 {
     struct promise_type 
     {
-        promise_type()                       { std::cout << "\npromise::promise"; } // step-1
-        future1 get_return_object()          { std::cout << "\npromise::ret_obj";   // step-2
-                                               return future1
-                                               { 
-                                                   std::coroutine_handle<promise_type>::from_promise(*this) 
-                                               };
-                                             }
-        std::suspend_never initial_suspend() { std::cout << "\npromise::initial"; return {}; } // step 4
-        std::suspend_never   final_suspend() { std::cout << "\npromise::final";   return {}; }
-        void unhandled_exception()           { }
+        promise_type()                       {   std::cout << "\npromise::promise";   } // step-1
+        future1 get_return_object()          {   std::cout << "\npromise::ret_obj";     // step-2
+                                                 return future1
+                                                 { 
+                                                     std::coroutine_handle<promise_type>::from_promise(*this) 
+                                                 };
+                                             }   
+        std::suspend_never initial_suspend() {   std::cout << "\npromise::initial"; return {};   } // step 4
+        std::suspend_never   final_suspend() {   std::cout << "\npromise::final";   return {};   }
+        void unhandled_exception()           {   }
     };
 
-    future1(std::coroutine_handle<promise_type> h_) : h(h_) // step-3
+    explicit future1(std::coroutine_handle<promise_type> h_) : h(h_) // step-3 
     {
         std::cout << "\nfuture1::future1"; 
     } 
@@ -99,10 +111,97 @@ struct future1
 }
 
 
-// ******************************** // 
-// *** Approach 2 (return data) *** //
-// ******************************** // 
+// ****************** // 
+// *** Approach 2 *** //
+// ****************** // 
+// How does coroutine get a coroutine handle (with another method)?
+// 1. coroutine invokes co_await operator and capture the return value
+// 2. define the return value in awaitable::resume
+//
+// Besides ...
+// coroutine_handle can be converted into promise by : p = coroutine_handle::promise()
+// promise can be converted into coroutine_handle by : h = coroutine_handle::from_promise(*this)
+// 
+// Now caller can access coroutine_handle,
+// coroutine can access coroutine_handle,
+// we form data connection between caller and coroutine, 
+// we can put data inside promise for communication.
+//
+// Here, in this example, we have 2 awaitable objects in coroutine.
+// 1. awaitable for getting promise in coroutine, called once only
+// 2. awaitable std::suspend_always for looping in coroutine
 
+template<typename T>
+struct future2 
+{
+    struct promise_type 
+    {
+        promise_type()                       {   std::cout << "\npromise::promise";   } // step-1
+        future2 get_return_object()          {   std::cout << "\npromise::ret_obj";     // step-2
+                                                 return future2
+                                                 { 
+                                                     std::coroutine_handle<promise_type>::from_promise(*this) 
+                                                 };
+                                             }   
+        std::suspend_never initial_suspend() {   std::cout << "\npromise::initial"; return {};   } // step 4
+        std::suspend_never   final_suspend() {   std::cout << "\npromise::final";   return {};   }
+        void unhandled_exception()           {   }
+
+        T data; // <--- new stuff (as compared to previous)
+    };
+
+    explicit future2(std::coroutine_handle<promise_type> h_) : h(h_) // step-3
+    {
+        std::cout << "\nfuture2::future2"; 
+    } 
+
+    // *** Conversion operator *** //
+    operator std::coroutine_handle<promise_type>() const // step-5
+    {
+        std::cout << "\nfuture2::convert_to_handle"; 
+        return h;
+    } 
+
+    std::coroutine_handle<promise_type> h;
+};
+
+template<typename T>
+struct awaitable2
+{
+    awaitable2() : p_ptr(nullptr) // step-5 
+    {
+        std::cout << "\nawaitable::awaitable"; 
+    }
+
+    bool await_ready() const noexcept { return false; }
+//  void await_suspend(std::coroutine_handle<> h) 
+    bool await_suspend(std::coroutine_handle<typename future2<T>::promise_type> h) // <--- need to add promise_type inside handle
+    {                                                                              //      dont forget typename ...
+        std::cout << "\nawaitable::suspend(handle)";
+        p_ptr = &h.promise();   
+
+        // We don't want suspension from this awaitable
+        // it offers an access to handle only.
+        return false; 
+    }
+    typename future2<T>::promise_type* await_resume() const noexcept
+    { 
+        return p_ptr;
+    }
+
+//  std::coroutine_handle<>* h_ptr; // use pointer to promise instead of pointer to handle 
+    future2<T>::promise_type* p_ptr;
+}; 
+
+template<typename T>
+[[nodiscard]] future2<T> coroutine2()
+{
+    for(std::uint32_t n=0;; ++n) 
+    {
+        co_await std::suspend_always{};
+        std::cout << "\ncoroutine1::iteration " << n << " with thread_id = " << std::this_thread::get_id();
+    }
+}
 
 // ********************* //
 // *** Test function *** //
@@ -127,9 +226,20 @@ void test_coroutine()
     for(int i=0; i<10; ++i) h1();
     std::cout << "\n\n";
 
+
+    // *** Approach 2 *** //
+    std::cout << "\n--------------------";
+    std::coroutine_handle<> h2 = coroutine2<std::uint32_t>();
+    std::cout << "\n--------------------";
+    std::cout << "\ncaller with main thread_id = " << std::this_thread::get_id();
+    for(int i=0; i<10; ++i) h2();
+    std::cout << "\n\n";
+
+
     // *** Explicit destroy handle in heap *** //
     h0.destroy();
     h1.destroy();
+    h2.destroy();
 }
 
 
